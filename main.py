18a9af4eb1e185fb8270807e78f7beaf6ccfac12
ciceroalvespro <143018315+ciceroalvespro@users.noreply.github.com>
2024-01-29 14:18:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -384,10 +384,10 @@ elif page == 'Ranking de Aeroportos':
         filtro_aeroportos = st.sidebar.selectbox("Aeroportos", df_anac["AERODROMO"].unique())
        
         df_anac_evo_pax = df_anac[df_anac["AERODROMO"] == filtro_aeroportos]
-        df_anac_evo_pax["ANO"].str.replace(",","").astype(int)
-        df_anac_evo_pax = df_anac_evo_pax.groupby("ANO")["PASSAGEIROS"].sum().reset_index()
+    
+        #df_anac_evo_pax = df_anac_evo_pax.groupby("ANO")["PASSAGEIROS"].sum().reset_index()
        
-        df_anac_evo_pax
+        fig_evo_pax = px.bar(df_anac_evo_pax, x="ANO", Y="PASSAGEIROS", title="Evolução na movimentação de passageiros")
         
      
      
